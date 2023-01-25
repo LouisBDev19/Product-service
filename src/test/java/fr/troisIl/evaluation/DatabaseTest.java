374fe10c95fad0c54b55c303de19caaf3ee3d44c
Louis Bayle <louis.bayledev@gmail.com>
2023-01-25 08:55:47 +0100

    Updating Exceptions DataBase

--- src/test/java/fr/troisIl/evaluation/DatabaseTest.java
+++ src/test/java/fr/troisIl/evaluation/DatabaseTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +13,7 @@ public class DatabaseTest {
     private Database db = null;
 
     @Before
-    public void setUp() {
+    public void setUp() throws SQLException {
         String testDatabaseFileName = "product.db";
 
         // reset la BDD avant le test
@@ -24,7 +25,7 @@ public class DatabaseTest {
     }
 
     @Test
-    public void testDatabaseTable() {
+    public void testDatabaseTable() throws SQLException {
         setUp();
 
         db.createBasicSqlTable();
